8903f10b2d43307fe4f4269914d50a1d5c15c516
mangyantribe <masangkaymm20@gmail.com>
2026-05-18 08:14:15 +0800

    country crud

--- resources/views/components/country/⚡delete.blade.php
+++ resources/views/components/country/⚡delete.blade.php
@@ -1,13 +1,60 @@
 <?php
 
 use Livewire\Component;
-
+use Livewire\Attributes\On;
+use App\Services\CountryService;
+use Flux\Flux;
 new class extends Component
 {
-    //
+    protected $countryService;
+    public $deleteCountryModal = false;
+    public $country;
+    public $country_id;
+
+    public function boot(CountryService $countryService)
+    {
+        $this->countryService = $countryService;
+    }
+
+    #[On('delete-country')]
+    public function deleteCountry($id)
+    {
+        $country = $this->countryService->findCountry($id);
+        $this->country = $country;
+        $this->deleteCountryModal = true;
+    }
+
+    public function confirmDeletion()
+    {
+        if($this->country->delete()){
+            Flux::toast( variant: 'success', heading: 'Deleted', text:'Country has been delete.');
+            $this->dispatch('refresh-countries');
+            $this->deleteCountryModal = false;
+        }
+    }
 };
 ?>
 
-<div>
-    {{-- If you do not have a consistent goal in life, you can not live it in a consistent way. - Marcus Aurelius --}}
-</div>
+
+<flux:modal wire:model.self="deleteCountryModal" class="min-w-[22rem]">
+    <div class="space-y-6">
+        <div>
+            <flux:heading size="lg">Delete {{ $country->name ?? "" }}?</flux:heading>
+
+            <flux:text class="mt-2">
+                You're about to delete this country.<br>
+                This action cannot be reversed.
+            </flux:text>
+        </div>
+
+        <div class="flex gap-2">
+            <flux:spacer />
+
+            <flux:modal.close>
+                <flux:button variant="ghost">Cancel</flux:button>
+            </flux:modal.close>
+
+            <flux:button wire:click="confirmDeletion" variant="danger">Delete</flux:button>
+        </div>
+    </div>
+</flux:modal>
--- resources/views/components/country/⚡list.blade.php
+++ resources/views/components/country/⚡list.blade.php
@@ -1,33 +1,57 @@
 <?php
 
 use Livewire\Component;
+use Livewire\WithPagination;
+use Livewire\Attributes\On;
 use Livewire\Attributes\Computed;
+use App\Services\CountryService;
 new class extends Component
 {
+    use WithPagination;
+    protected $countryService;
+
+    public function boot(CountryService $countryService)
+    {
+        $this->countryService = $countryService;
+    }
+
+    #[On('refresh-countries')] 
     #[Computed]
     public function countries()
     {
-        return [];
+        return $this->countryService->getCounties();
+    }
+
+    public function editCountry($id)
+    {
+        $this->dispatch('edit-country',id:$id);
+    }
+
+    public function deleteCountry($id)
+    {
+        $this->dispatch('delete-country',id:$id);
     }
 };
 ?>
 
-<flux:table>
+<flux:table :paginate="$this->countries">
     <flux:table.columns>
         <flux:table.column>Photo</flux:table.column>
         <flux:table.column>Name</flux:table.column>
+        <flux:table.column align="end"></flux:table.column>
     </flux:table.columns>
 
     <flux:table.rows>
         @foreach ($this->countries as $country)
             <flux:table.row :key="$country->id">
                 <flux:table.cell class="flex items-center gap-3">
-                    <flux:avatar size="xs" color="auto" color:seed="{{ $country->id }}"/>
+                    <flux:avatar circle color="auto" color:seed="{{ $country->id }}" :name="$country->name"/>
                 </flux:table.cell>
-                <flux:table.cell variant="strong">{{ $order->amount }}</flux:table.cell>
+                <flux:table.cell variant="strong">{{ $country->name }}</flux:table.cell>
 
-                <flux:table.cell>
-                    <flux:button variant="ghost" size="sm" icon="ellipsis-horizontal" inset="top bottom"></flux:button>
+                <flux:table.cell align="end">
+                    <flux:button variant="primary" wire:click="editCountry({{ $country->id }})">Edit</flux:button>
+                    <flux:button variant="danger" wire:click="deleteCountry({{ $country->id }})">Delete</flux:button>
                 </flux:table.cell>
             </flux:table.row>
         @endforeach
